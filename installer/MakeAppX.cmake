3ded17855f9e37bfb5ea9eedee60df07085e0fde
sergey radionov <rsatom@gmail.com>
2025-10-01 15:26:47 +0700

    cmake, win: initial install support added

---
+++ installer/MakeAppX.cmake
@@ -0,0 +1,15 @@
+#"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\makeappx.exe" pack /d ./install /p RTMPVideoStreamer.appx /o
+
+find_program(
+    MAKEAPPX MakeAppx.exe
+    HINTS "$ENV{ProgramFiles\(x86\)}/Windows Kits/10/App Certification Kit"
+    REQUIRED
+)
+
+set(APPX_TARGET_PATH "${CPACK_TOPLEVEL_DIRECTORY}/RTMPVideoStreamer-${CPACK_PACKAGE_VERSION}.appx")
+
+execute_process(
+    COMMAND "${MAKEAPPX}" pack /d "${CPACK_TEMPORARY_DIRECTORY}" /p "${APPX_TARGET_PATH}" /o
+)
+
+set(CPACK_EXTERNAL_BUILT_PACKAGES "${APPX_TARGET_PATH}")
